25871207fc1eb16386f5df1c9db789abddfe1425
Mehmet Coşkun <mehmet@coskun.dev>
2023-07-16 15:54:11 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/joho/godotenv"
+	twitterscraper "github.com/n0madic/twitter-scraper"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+	scraper := twitterscraper.New()
+	scraper.LoginOpenAccount()
+	profile, err := scraper.GetProfile(os.Getenv("TWITTER_USERNAME"))
+	if err != nil {
+		panic(err)
+	}
+	bio := profile.Biography
+	re := regexp.MustCompile(`(https?://[^\s]+)`)
+	url := re.FindString(bio)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, url, http.StatusMovedPermanently)
+	})
+
+	fmt.Println("Listening on port 8080...")
+	http.ListenAndServe(":8080", nil)
+}
